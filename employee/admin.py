6443b0934a7c8eaf5e51f5c4b6d42481fbc8e9ab
ashkantaravati <ashkan.taravati@gmail.com>
2022-10-14 17:05:27 +0330

    add leave request #3

--- employee/admin.py
+++ employee/admin.py
@@ -1,6 +1,8 @@
+from atexit import register
 import imp
 from django.contrib import admin
-from .models import Employee
+from .models import Employee, LeaveRequest
 
 # Register your models here.
 admin.site.register(Employee)
+admin.site.register(LeaveRequest)
--- employee/models.py
+++ employee/models.py
@@ -12,3 +12,30 @@ class Employee(models.Model):
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name
+
+
+class LeaveRequest(models.Model):
+    class DurationType(models.IntegerChoices):
+        Hours = 1
+        Days = 2
+
+    starting_from = models.DateTimeField()
+    duration_type = models.IntegerField(choices=DurationType.choices)
+    duration = models.SmallIntegerField()
+
+    employee = models.ForeignKey(
+        to=Employee, on_delete=models.CASCADE, related_name="leave_requests"
+    )
+    substitute = models.ForeignKey(
+        to=Employee,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="substitute_accountabilities",
+    )
+    reason = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
